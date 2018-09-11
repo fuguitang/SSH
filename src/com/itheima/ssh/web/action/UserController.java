67d27ec8f2cea94a8890a0f0c9251f0c8f44b1f0
fuguitang <tangfugui_iec@126.com>
2018-09-11 15:29:08 +0800

    第二版

---
+++ src/com/itheima/ssh/web/action/UserController.java
@@ -0,0 +1,10 @@
+package com.itheima.ssh.web.action;
+
+/**
+ * @ClassName UserController
+ * @Description TODO
+ * @Author tangfugui
+ * @Date 2018/9/11 15:28
+ **/
+public class UserController {
+}
